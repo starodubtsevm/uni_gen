875f01aaf974c06dbbac0181037b48cef7dd733a
starodubtsevm <starodubtsevm@gmail.com>
2020-03-06 15:55:07 +0500

    работы в test.py

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 N =20000
 imp_duty_count = 0
 count_bit = 8
-Code_alsen1 = 16
-Code_alsen2 = 0
+Code_alsen1 = 10
+Code_alsen2 = 11
 Byte1 = Code_alsen1
 Byte2 = Code_alsen2
+diBit = 0
+phase = 0
+d_phase = 0
 
 for i in range(N):
 
@@ -13,7 +16,21 @@ for i in range(N):
       else:
          imp_duty_count=0
          diBit=((Byte1 & 0x80)>> 6)+((Byte2 & 0x80)>>7)
-         print(diBit,Byte1,Byte2)
+
+         if diBit == 0:
+            d_phase = 0
+            print("phase +0")
+         if diBit == 1:
+            d_phase = 90
+            print("phase +90")
+         if diBit == 3:
+            d_phase = 180
+            print("phase +180")
+         if diBit == 2:
+            d_phase = 270
+            print("phase +270")
+         phase = phase + d_phase
+         print(diBit, d_phase, phase)
          Byte1=Byte1<<1
          Byte2=Byte2<<1
 
